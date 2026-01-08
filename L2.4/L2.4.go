80abab7ddee1376dfef8062896da49df8fd9f191
Мармулев Семён <marmulev2003@mail.ru>
2026-01-08 21:31:35 +0300

    /

--- L2.4/L2.4.go
+++ L2.4/L2.4.go
@@ -3,7 +3,7 @@ package main
 func main() {
 	ch := make(chan int)
 	go func() {
-		defer close(ch) // закрытие канала
+		defer close(ch) // закрытие каналаx
 		for i := 0; i < 10; i++ {
 			ch <- i
 		}
